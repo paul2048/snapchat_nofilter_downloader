4a5729af363e817061d8c146d834ffad7417a148
Paul <mihali.paul123@gmail.com>
2020-09-07 12:50:24 +0100

    Bug fixes and code improvements

--- app.py
+++ app.py
@@ -1,40 +1,86 @@
 import requests
 import os
-import re
+import piexif
+import zipfile
 
-from requests_testadapter import Resp
+from PIL import Image
 from bs4 import BeautifulSoup
+from datetime import datetime
+from win32_setctime import setctime
 
 
-# 
-with open("html/memories_history.html") as f:
-    soup = BeautifulSoup(f.read(), "lxml")
-    
-# 
-dates = [td.text for td in soup.select("tr td:first-child")]
-media_types = [td.text for td in soup.select("tr td:nth-child(2)")]
-auth_links = [
-    # Extract the URL from the string
-    anchor["href"][29:-3]
-    for anchor in soup.select("tr td:nth-child(3) a")
-]
+def main():
+    # Get the location of the zip file from Snapchat
+    mydata = [
+        fname for fname in os.listdir("./")
+        if "mydata_" in fname
+    ][0]
 
-for date, media_type, auth_link in zip(dates, media_types, auth_links):
-    # Get the download link of the current photo/video 
-    dl_link = requests.post(auth_link).text
-    # The file extension of the current photo/video
-    extension = ".jpg" if media_type == "PHOTO" else ".mp4"
-
-    # Create a media folder if it doesn't exist already
+    # Unzip the dowloaded zip file from Snapchat
     try:
-        os.mkdir("media")
-    except FileExistsError:
-        pass
+        with zipfile.ZipFile(mydata, 'r') as zip_ref:
+            zip_ref.extractall("mydata")
+    except FileNotFoundError:
+        print("The zip file from Snapchat can't be found.")
+        return
 
-    # 
-    with open(f"media/{date}{extension}", "wb") as f:
+    # Prepare the scraping of "memories_history.html"
+    with open("mydata/html/memories_history.html", "r") as f:
+        soup = BeautifulSoup(f.read(), "lxml")
+
+    # Scrape each column of the table from "memories_history.html"
+    c1 = soup.select("tr td:nth-child(1)")
+    c2 = soup.select("tr td:nth-child(2)")
+    c3 = soup.select("tr td:nth-child(3) a")
+
+    # Get the timestamp, extension, and authentication link for each file
+    timestamps = [td.text.replace(":", "-")[:-4] for td in c1]
+    extensions = [".jpg" if td.text == "PHOTO" else ".mp4" for td in c2]
+    auth_links = [anchor["href"][29:-3] for anchor in c3]
+
+    for timestamp, extension, auth_link in zip(timestamps, extensions, auth_links):
+        # Get the download link of the current image/video 
+        dl_link = requests.post(auth_link).text
+
+        # Create a "media" directory if it doesn't exist already
         try:
-            response = requests.get(dl_link)
-            f.write(response.content)
-        except requests.exceptions.MissingSchema:
+            os.mkdir("media")
+        except FileExistsError:
             pass
+
+        # Download and save the image/video in the "media" directory
+        with open(f"media/{timestamp}{extension}", "wb") as f:
+            try:
+                # Download the image/video
+                response = requests.get(dl_link)
+                f.write(response.content)
+
+                # Edit the date and time of the time when the image was taken
+                if extension == ".jpg":
+                    img = Image.open(f"media/{timestamp}{extension}")
+                    # Create an empty exif data and assign it to the image because
+                    # the files downloaded from Snapchat don't have exif data
+                    img.info["exif"] = piexif.dump({})
+                    # Convert the exif data from `bytes` to `dict`
+                    exif_dict = piexif.load(img.info["exif"])
+                    # Assign the correct date and time to the exif `dict`
+                    new_timestamp = timestamp.replace("-", ":")
+                    exif_dict["Exif"][piexif.ExifIFD.DateTimeOriginal] = new_timestamp
+                    # Convert the exif `dict` back to `bytes` and save the image
+                    exif_bytes = piexif.dump(exif_dict)
+                    img.save(f"media/{timestamp}{extension}", "jpeg", exif=exif_bytes)
+                # Edit the date and time of the time when the video was recorded
+                else:
+                    # The number of seconds from 1970:01:01 00:00:00 UTC to the
+                    # recording date (required as the second argument for `setctime`)
+                    secs = datetime.strptime(timestamp, "%Y-%m-%d %H-%M-%S").timestamp()
+                    # Save the video in the media directory
+                    setctime(f"media/{timestamp}{extension}", secs)
+                print(f"Downloaded {timestamp}{extension}")
+            # If `dl_link` is an invalid URL
+            except requests.exceptions.MissingSchema:
+                print(f"Can't download {timestamp}{extension}")
+
+
+if __name__ == "__main__":
+    main()
