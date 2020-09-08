8ae44300c206058939a9692940d24da6c462cd49
Paul <mihali.paul123@gmail.com>
2020-09-08 13:40:58 +0100

    Version 1.0

---
+++ README.md
@@ -0,0 +1,25 @@
+# snapchat_nofilter_downloader
+![app_demo](demo.jpg)
+## Features
+- It downloads the images from your snapchat memories (your media files stored on Snapchat's servers) with the filters removed.
+    - Includes removal of:
+    ```diff
+    + color filters
+    + location filters
+    + stickers
+    + drawings
+    + text
+    ```
+    - Excludes removal of:
+    ```diff
+    - lenses
+    ```
+- It changes the Exif data of each image so that it corresponds with the rough date and time of when each image was taken (the default date and time is the date and time when the file was downloaded).
+- It keeps track of which image was, and which image wasn't downloaded, and how many of them were or weren't downloaded.
+
+## How to use
+1. Request your Snapchat data within the app by going to Settings -> My Data -> Submit Request
+2. Download your data from your your email.
+3. Move the downloaded zip file inside of the `snapchat_nofilter_downloader` directory.
+4. Run `python app.py` in your terminal.
+5. Wait for your images to be downloaded inside the `media` directory.
--- app.py
+++ app.py
@@ -6,13 +6,12 @@ import zipfile
 from PIL import Image
 from bs4 import BeautifulSoup
 from datetime import datetime
-from win32_setctime import setctime
 
 
 def main():
-    # Keep track of the number of downloads of images and videos
+    # Keep track of the number of images downloaded and not downloaded
     images_dls = 0
-    videos_dls = 0
+    error_dls = 0
 
     # Get the location of the zip file from Snapchat
     mydata = [
@@ -37,31 +36,30 @@ def main():
     c2 = soup.select("tr td:nth-child(2)")
     c3 = soup.select("tr td:nth-child(3) a")
 
-    # Get the timestamp, extension, and authentication link for each file
+    # Get the timestamp, media type, and authentication link for each file
     timestamps = [td.text.replace(":", "-")[:-4] for td in c1]
-    extensions = [".jpg" if td.text == "PHOTO" else ".mp4" for td in c2]
+    media_types = [td.text for td in c2]
     auth_links = [anchor["href"][29:-3] for anchor in c3]
 
-    for timestamp, extension, auth_link in zip(timestamps, extensions, auth_links):
-        # Get the download link of the current image/video 
-        dl_link = requests.post(auth_link).text
+    for timestamp, media_type, auth_link in zip(timestamps, media_types, auth_links):
+        if media_type == "PHOTO":
+            # Get the download link of the current image/video 
+            dl_link = requests.post(auth_link).text
 
-        # Create a "media" directory if it doesn't exist already
-        try:
-            os.mkdir("media")
-        except FileExistsError:
-            pass
-
-        # Download and save the image/video in the "media" directory
-        with open(f"media/{timestamp}{extension}", "wb") as f:
+            # Create a "media" directory if it doesn't exist already
             try:
-                # Download the image/video
-                response = requests.get(dl_link)
-                f.write(response.content)
+                os.mkdir("media")
+            except FileExistsError:
+                pass
 
-                # Edit the date and time of the time when the image was taken
-                if extension == ".jpg":
-                    img = Image.open(f"media/{timestamp}{extension}")
+            # Download and save the image in the "media" directory
+            with open(f"media/{timestamp}.jpg", "wb") as f:
+                try:
+                    # Download the image
+                    response = requests.get(dl_link)
+                    f.write(response.content)
+
+                    img = Image.open(f"media/{timestamp}.jpg")
                     # Create an empty exif data and assign it to the image because
                     # the files downloaded from Snapchat don't have exif data
                     img.info["exif"] = piexif.dump({})
@@ -72,25 +70,16 @@ def main():
                     exif_dict["Exif"][piexif.ExifIFD.DateTimeOriginal] = new_timestamp
                     # Convert the exif `dict` back to `bytes` and save the image
                     exif_bytes = piexif.dump(exif_dict)
-                    img.save(f"media/{timestamp}{extension}", "jpeg", exif=exif_bytes)
+                    img.save(f"media/{timestamp}.jpg", "jpeg", exif=exif_bytes)
                     images_dls += 1
-                # Edit the date and time of the time when the video was recorded
-                else:
-                    # The number of seconds from 1970:01:01 00:00:00 UTC to the
-                    # recording date (required as the second argument for `setctime`)
-                    secs = datetime.strptime(timestamp, "%Y-%m-%d %H-%M-%S").timestamp()
-                    # Save the video in the media directory
-                    setctime(f"media/{timestamp}{extension}", secs)
-                    videos_dls += 1
-                print(f"Downloaded {timestamp}{extension}")
-            # If `dl_link` is an invalid URL or users's OS is not campatible
-            # (videos can only be saved on Windows with the code above)
-            except (requests.exceptions.MissingSchema, OSError):
-                print(f"Can't download {timestamp}{extension}")
+                    print(f"Downloaded {timestamp}.jpg")
+                # If `dl_link` is an invalid URL
+                except requests.exceptions.MissingSchema:
+                    error_dls += 1
+                    print(f"Can't download {timestamp}.jpg")
 
-    print(f"Images downloaded: {images_dls}")
-    print(f"Videos downloaded: {videos_dls}")
-    print(f"Total downloads:   {images_dls + videos_dls}")
+    print(f"Images downloaded:      {images_dls}")
+    print(f"Unsuccessful downloads: {error_dls}")
 
 
 if __name__ == "__main__":
